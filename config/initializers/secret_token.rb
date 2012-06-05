d72bffde95f43e60e91084bce02714ec79d290eb
CosmicCat <cosmic.cat144@gmail.com>
2012-06-05 11:29:27 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mapblog::Application.config.secret_token = 'dcdc81d61e3b20797fd599f20e223d16870f0058e9f9f349f594b85bbd49628cae4a9cea3a9383e93639dfcd83082ca076a0d69aab59f420d61e70e86536b08d'
